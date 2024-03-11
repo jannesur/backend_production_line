47bd61279426e8213f08e238436994b59a9d113f
jannesur <127102006+jannesur@users.noreply.github.com>
2024-03-11 13:10:09 +0100

    Add files via upload
    initial commit

---
+++ productionline/src/main/java/de/vw/productionline/productionline/ProductionlineApplication.java
@@ -0,0 +1,13 @@
+package de.vw.productionline.productionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionlineApplication.class, args);
+	}
+
+}
---
+++ productionline/src/test/java/de/vw/productionline/productionline/ProductionlineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vw.productionline.productionline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
