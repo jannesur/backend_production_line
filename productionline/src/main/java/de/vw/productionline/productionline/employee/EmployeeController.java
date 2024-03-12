8fd2d5d3b963e16eddfb6f684c318a9c8de65b09
Schwalm, Christopher (SE-A/35) <christopher.schwalm@volkswagen.de>
2024-03-12 13:45:14 +0100

    added endpoints for employee, documentaion is in miro

--- productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeController.java
+++ productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeController.java
@@ -1,12 +1,47 @@
 package de.vw.productionline.productionline.employee;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("employees")
 public class EmployeeController {
     private EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("all")
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        if (employees.isEmpty()) {
+            throw new IllegalArgumentException("No employees found");
+        }
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("without-station")
+    public List<Employee> getAllEmployeesWithoutStation() {
+        List<Employee> employees = employeeService.getAllEmployeesWithoutStation();
+        if (employees.isEmpty()) {
+            throw new IllegalArgumentException("No employees without station found");
+        }
+        return employees;
+    }
+
+    @GetMapping("id")
+    public Employee getEmployeeById(@RequestParam UUID uuid) {
+        Optional<Employee> employee = employeeService.getEmployeeById(uuid);
+        if (employee.isEmpty()) {
+            throw new IllegalArgumentException("Employee not found");
+        }
+        return employee.get();
     }
 }
