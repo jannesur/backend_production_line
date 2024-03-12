a174676d7eea31f59186e71986875634f4ae5c1f
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2024-03-12 14:11:39 +0100

    Include exception handling for ObjectNotFoundException

--- productionline/src/main/java/de/vw/productionline/productionline/exceptions/GlobalExceptionHandler.java
+++ productionline/src/main/java/de/vw/productionline/productionline/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package de.vw.productionline.productionline.exceptions;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleObjectNotFoundException(ObjectNotFoundException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
 }
