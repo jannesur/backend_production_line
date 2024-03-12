d9823398c7ce1998a5789c97c92ebeaf8debd958
Surborg, Janne (SE-A/34) <janne.surborg@volkswagen.de>
2024-03-12 10:01:23 +0100

    add enums, controller, repository and service.

--- productionline/src/main/java/de/vw/productionline/productionline/productionline/ProductionLine.java
+++ productionline/src/main/java/de/vw/productionline/productionline/productionline/ProductionLine.java
@@ -1,5 +1,80 @@
 package de.vw.productionline.productionline.productionline;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class ProductionLine {
+    @Id
+    private UUID uuid = UUID.randomUUID();
+    private String name;
+    private Status status;
+    private SimulationStatus simulationStatus;
+    private VehicleModel vehicleModel;
+
+    public ProductionLine() {
+
+    }
+
+    public ProductionLine(UUID uuid, String name, Status status, SimulationStatus simulationStatus, VehicleModel vehicleModel) {
+        this.uuid = uuid;
+        this.name = name;
+        this.status = status;
+        this.simulationStatus = simulationStatus;
+        this.vehicleModel = vehicleModel;
+    }
+
+
+    public UUID getUuid() {
+        return this.uuid;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public SimulationStatus getSimulationStatus() {
+        return this.simulationStatus;
+    }
+
+    public void setSimulationStatus(SimulationStatus simulationStatus) {
+        this.simulationStatus = simulationStatus;
+    }
+
+    public VehicleModel getVehicleModel() {
+        return this.vehicleModel;
+    }
+
+    public void setVehicleModel(VehicleModel vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " uuid='" + getUuid() + "'" +
+            ", name='" + getName() + "'" +
+            ", status='" + getStatus() + "'" +
+            ", simulationStatus='" + getSimulationStatus() + "'" +
+            ", vehicleModel='" + getVehicleModel() + "'" +
+            "}";
+    }
+
     
+
 }
---
+++ productionline/src/main/java/de/vw/productionline/productionline/productionline/ProductionLineService.java
@@ -0,0 +1,8 @@
+package de.vw.productionline.productionline.productionline;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductionLineService {
+    
+}
