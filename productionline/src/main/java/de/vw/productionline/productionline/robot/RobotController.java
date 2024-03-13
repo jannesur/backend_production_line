36a850c4508528ad0be89db64e0e4e4f1244473f
Surborg, Janne (SE-A/34) <janne.surborg@volkswagen.de>
2024-03-13 15:12:20 +0100

    add methods in controller and service class.

--- productionline/src/main/java/de/vw/productionline/productionline/robot/RobotController.java
+++ productionline/src/main/java/de/vw/productionline/productionline/robot/RobotController.java
@@ -1,12 +1,54 @@
 package de.vw.productionline.productionline.robot;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
+@RequestMapping("/robots")
 public class RobotController {
     private RobotService robotService;
 
     public RobotController(RobotService robotService) {
         this.robotService = robotService;
+    }
+
+    @GetMapping()
+    public List<Robot> getAllRobots() {
+        return robotService.getAllRobots();
+    }
+
+    @GetMapping("/{uuid}")
+    public Robot getRobotById(@PathVariable(value = "uuid") UUID uuid) {
+        return robotService.getRobotById(uuid);
+    }
+
+    @PostMapping()
+    public Robot createRobot(@RequestBody Robot robot) {
+        return robotService.createRobot(robot);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public void deleteRobot(@PathVariable(value = "uuid") UUID uuid) {
+        robotService.deleteRobot(uuid);
+    }
+
+    @GetMapping("without")
+    public List<Robot> getRobotsWithoutProductionLine() {
+        return robotService.getAllRobotsNotInProductionLine();
+    }
+    
+    @PutMapping("/{uuid}")
+    public Robot updateRobot(@PathVariable UUID uuid, @RequestBody Robot updatedRobot) {
+        return robotService.updateRobot(uuid, updatedRobot);
     }
 }
--- productionline/src/main/java/de/vw/productionline/productionline/robot/RobotService.java
+++ productionline/src/main/java/de/vw/productionline/productionline/robot/RobotService.java
@@ -1,6 +1,11 @@
 package de.vw.productionline.productionline.robot;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
+
+import de.vw.productionline.productionline.exceptions.ObjectNotFoundException;
 
 @Service
 public class RobotService {
@@ -10,4 +15,45 @@ public class RobotService {
         this.robotRepository = robotRepository;
     }
 
+    public List<Robot> getAllRobots() {
+        return robotRepository.findAll();
+    }
+
+    public Robot getRobotById(UUID uuid) {
+        Optional<Robot> robot = robotRepository.findById(uuid);
+        if(robot.isEmpty()) {
+            throw new ObjectNotFoundException("Robot not found");
+        }
+        return robot.get();
+    }
+
+    public Robot createRobot(Robot robot) {
+        return robotRepository.save(robot);
+    }
+
+    public void deleteRobot(UUID uuid) {
+        robotRepository.deleteById(uuid);
+    }
+
+    public List<Robot> getAllRobotsNotInProductionLine() {
+        List<Robot> robots = this.robotRepository.findAll();
+        return robots.stream().filter(robot -> robot.getProductionLine() != null).toList();
+    }
+
+    public Robot updateRobot(UUID uuid, Robot robot) {
+        Optional<Robot> optionalRobot = robotRepository.findById(uuid);
+        if (optionalRobot.isEmpty()) {
+            throw new ObjectNotFoundException("Robot not found");
+        }
+        Robot existingRobot = optionalRobot.get();
+        existingRobot.setName(robot.getName());
+        existingRobot.setDurationInMinutes(robot.getDurationInMinutes());
+        existingRobot.setFailureProbability(robot.getFailureProbability());
+        existingRobot.setTimeToRecovery(robot.getTimeToRecovery());
+        existingRobot.setProductionLine(robot.getProductionLine());
+        existingRobot.setMaintenanceCycleInMinutes(robot.getMaintenanceCycleInMinutes());
+        existingRobot.setMaintenanceTimeInMinutes(robot.getMaintenanceTimeInMinutes());
+        return robotRepository.save(existingRobot);
+    }
 }
+
