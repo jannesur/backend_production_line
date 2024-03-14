80cae7363e0bb513814fd52302355c12a2aa4cd0
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2024-03-14 13:27:00 +0100

    Testing§

--- productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeService.java
+++ productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeService.java
@@ -1,5 +1,6 @@
 package de.vw.productionline.productionline.employee;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import de.vw.productionline.productionline.exceptions.ObjectNotFoundException;
 import de.vw.productionline.productionline.station.Station;
 import de.vw.productionline.productionline.station.StationRepository;
+import de.vw.productionline.productionline.station.StationService;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class EmployeeService {
@@ -18,9 +21,38 @@ public class EmployeeService {
 
     private StationRepository stationRepository;
 
-    public EmployeeService(EmployeeRepository employeeRepository, StationRepository stationRepository) {
+    private StationService stationService;
+
+    public EmployeeService(EmployeeRepository employeeRepository, StationRepository stationRepository,
+            StationService stationService) {
         this.employeeRepository = employeeRepository;
         this.stationRepository = stationRepository;
+        this.stationService = stationService;
+    }
+
+    @PostConstruct
+    private void initializeData() {
+        System.out.println("Initializing employees");
+        Station station = stationRepository.getByName("Painting").get();
+        Employee employee1 = new Employee("Janne", station);
+        Employee employee2 = new Employee("Adriana", station);
+        Employee employee3 = new Employee("Tim", station);
+        Employee employee4 = new Employee("Chris", null);
+        Employee employee5 = new Employee("Leon", null);
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
+        employeeRepository.save(employee4);
+        employeeRepository.save(employee5);
+
+        Set<Employee> employees = new HashSet<>();
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+
+        // station.setEmployees(employees);
+        // stationRepository.save(station);
     }
 
     public Employee getEmployeeById(UUID uuid) {
@@ -56,9 +88,9 @@ public class EmployeeService {
         employeeRepository.deleteById(uuid);
     }
 
-    public Employee updatEmployee (UUID uuid, Employee employee) {
+    public Employee updatEmployee(UUID uuid, Employee employee) {
         Optional<Employee> optionalEmployee = employeeRepository.findById(uuid);
-        if(optionalEmployee.isEmpty()) {
+        if (optionalEmployee.isEmpty()) {
             throw new ObjectNotFoundException("Employee not found.");
         }
         Employee existingEmployee = optionalEmployee.get();
