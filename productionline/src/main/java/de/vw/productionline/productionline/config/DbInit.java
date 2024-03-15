e7e3ff9c5011e967b52c31f84754dd5a4d4fcf58
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2024-03-15 08:22:55 +0100

    Add new objects to the database

--- productionline/src/main/java/de/vw/productionline/productionline/config/DbInit.java
+++ productionline/src/main/java/de/vw/productionline/productionline/config/DbInit.java
@@ -3,11 +3,16 @@ package de.vw.productionline.productionline.config;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import de.vw.productionline.productionline.employee.Employee;
 import de.vw.productionline.productionline.employee.EmployeeRepository;
+import de.vw.productionline.productionline.productionline.ProductionLine;
 import de.vw.productionline.productionline.productionline.ProductionLineRepository;
+import de.vw.productionline.productionline.productionline.VehicleModel;
+import de.vw.productionline.productionline.robot.Robot;
 import de.vw.productionline.productionline.robot.RobotRepository;
 import de.vw.productionline.productionline.station.Station;
 import de.vw.productionline.productionline.station.StationRepository;
@@ -19,6 +24,7 @@ public class DbInit {
     private ProductionLineRepository productionLineRepository;
     private RobotRepository robotRepository;
     private StationRepository stationRepository;
+    private Logger logger = LoggerFactory.getLogger(DbInit.class);
 
     public DbInit(EmployeeRepository employeeRepository, ProductionLineRepository productionLineRepository,
             RobotRepository robotRepository, StationRepository stationRepository) {
@@ -39,7 +45,7 @@ public class DbInit {
     }
 
     private void saveNewStations() {
-        System.out.println("Initializing stations");
+        logger.info("Initializing stations");
         Station station1 = new Station("Painting", 10l, 0.5, 15);
         Station station2 = new Station("Wheel", 1l, 0.01, 10);
         Station station3 = new Station("Tires", 5l, 0.05, 150);
@@ -54,7 +60,7 @@ public class DbInit {
     }
 
     private void saveNewEmployees() {
-        System.out.println("Initializing employees");
+        logger.info("Initializing employees");
         Employee employee1 = new Employee("Janne", null);
         Employee employee2 = new Employee("Adriana", null);
         Employee employee3 = new Employee("Tim", null);
@@ -69,14 +75,38 @@ public class DbInit {
     }
 
     private void saveNewRobots() {
+        logger.info("Initializing robots.");
+        Robot robot1 = new Robot("Robby Robot", 15l, 0.25, 20l, 120l, 15l);
+        Robot robot2 = new Robot("Painting Robot", 10l, 0.01, 30l, 320l, 10l);
+        Robot robot3 = new Robot("Wheels Robot", 1l, 0.2, 10l, 360l, 5l);
+        Robot robot4 = new Robot("Bumper Robot", 150l, 0.001, 1l, 30l, 30l);
+        Robot robot5 = new Robot("VW Robot", 25l, 0.4, 5l, 40l, 25l);
 
+        robotRepository.save(robot1);
+        robotRepository.save(robot2);
+        robotRepository.save(robot3);
+        robotRepository.save(robot4);
+        robotRepository.save(robot5);
     }
 
     private void saveNewProductionLines() {
+        logger.info("Initializing production lines.");
+        // String name, VehicleModel vehicleModel
+        ProductionLine productionLine1 = new ProductionLine("Production line 1", VehicleModel.GOLF);
+        ProductionLine productionLine2 = new ProductionLine("Production line 2", VehicleModel.ID3);
+        ProductionLine productionLine3 = new ProductionLine("Production line 3", VehicleModel.ID4);
+        ProductionLine productionLine4 = new ProductionLine("Production line 4", VehicleModel.ID7);
+        ProductionLine productionLine5 = new ProductionLine("Production line 5", VehicleModel.GOLF);
 
+        productionLineRepository.save(productionLine1);
+        productionLineRepository.save(productionLine2);
+        productionLineRepository.save(productionLine3);
+        productionLineRepository.save(productionLine4);
+        productionLineRepository.save(productionLine5);
     }
 
     private void assignEmployeesToStations() {
+        logger.info("Assigning employees to stations.");
         Set<Employee> employees = new HashSet<>();
 
         Employee printEmployee = employeeRepository.getByName("Janne").get();
@@ -94,6 +124,7 @@ public class DbInit {
     }
 
     private void assignStationsAndRobotsToProductionLines() {
+        logger.info("Assigning stations and robots to production lines.");
 
     }
 
