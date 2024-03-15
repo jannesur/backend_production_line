b3f32a29582d286679570502ccadad8b0c687dc6
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2024-03-15 09:13:10 +0100

    Update repositories to use method findByName(), link stations and robots to production lines in sample data

--- productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeRepository.java
+++ productionline/src/main/java/de/vw/productionline/productionline/employee/EmployeeRepository.java
@@ -6,5 +6,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-    public Optional<Employee> getByName(String name);
+    public Optional<Employee> findByName(String name);
 }
